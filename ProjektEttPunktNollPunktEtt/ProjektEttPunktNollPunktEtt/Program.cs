4bc579f49e5ce18af7febf9b2954e8b10b3bdf3a
Linus Hagstrand <spel.nordboy@hotmail.com>
2016-08-29 17:16:56 +0200

    Potato

---
+++ ProjektEttPunktNollPunktEtt/ProjektEttPunktNollPunktEtt/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektEttPunktNollPunktEtt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Beep();
+            Console.Beep();
+            Console.ReadKey();
+        }
+    }
+}
